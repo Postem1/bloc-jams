38c4335419fc5884e4cbddb00f2c022fb433265d
Steeve Postemus <spostemus@gmail.com>
2016-12-06 12:35:31 -0800

    checkpoint 30 completed

--- scripts/album.js
+++ scripts/album.js
@@ -1,84 +1,85 @@
-//create Javascript objects to represent albums
-var albumPicasso = {
-    title: 'The Colors',
-    artist: 'Pablo Picasso',
-    label: 'Cubism',
-    year: '1881',
-    albumArtUrl: 'assets/images/album_covers/01.png',
-    songs: [
-        { title: 'Blue', duration: '4:26' },
-        { title: 'Green', duration: '3:14' },
-        { title: 'Red', duration: '5:01' },
-        { title: 'Pink', duration: '3:21'},
-        { title: 'Magenta', duration: '2:15'}
-    ]
-};
+    //create Javascript objects to represent albums
+    var albumPicasso = {
+        title: 'The Colors',
+        artist: 'Pablo Picasso',
+        label: 'Cubism',
+        year: '1881',
+        albumArtUrl: 'assets/images/album_covers/01.png',
+        songs: [
+            { title: 'Blue', duration: '4:26' },
+            { title: 'Green', duration: '3:14' },
+            { title: 'Red', duration: '5:01' },
+            { title: 'Pink', duration: '3:21'},
+            { title: 'Magenta', duration: '2:15'}
+        ]
+    };
 
-var albumMarconi = {
-    title: 'The Telephone',
-    artist: 'Guglielmo Marconi',
-    label: 'EM',
-    year: '1909',
-    albumArtUrl: 'assets/images/album_covers/20.png',
-    songs: [
-        { title: 'Hello, Operator?', duration: '1:01' },
-        { title: 'Ring, ring, ring', duration: '5:01' },
-        { title: 'Fits in your pocket', duration: '3:21'},
-        { title: 'Can you hear me now?', duration: '3:14' },
-        { title: 'Wrong phone number', duration: '2:15'}
-    ]
-};
+    var albumMarconi = {
+        title: 'The Telephone',
+        artist: 'Guglielmo Marconi',
+        label: 'EM',
+        year: '1909',
+        albumArtUrl: 'assets/images/album_covers/20.png',
+        songs: [
+            { title: 'Hello, Operator?', duration: '1:01' },
+            { title: 'Ring, ring, ring', duration: '5:01' },
+            { title: 'Fits in your pocket', duration: '3:21'},
+            { title: 'Can you hear me now?', duration: '3:14' },
+            { title: 'Wrong phone number', duration: '2:15'}
+        ]
+    };
 
-//function to generate song row content
-// will use information stored in album objects
-var createSongRow = function(songNumber, songName, songLength) {
-    var template =
-        '<tr class="album-view-song-item">'
-        + '  <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>'
-        + '  <td class="song-item-title">' + songName + '</td>'
-        + '  <td class="song-item-duration">' + songLength + '</td>'
-        + '</tr>'
-    ;
-    return template;
-};
+    //function to generate song row content
+    // will use information stored in album objects
+    var createSongRow = function(songNumber, songName, songLength) {
+        var template =
+            '<tr class="album-view-song-item">'
+            + '  <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>'
+            + '  <td class="song-item-title">' + songName + '</td>'
+            + '  <td class="song-item-duration">' + songLength + '</td>'
+            + '</tr>'
+        ;
+        return $(template);
+    };
 
-//Will take one our album objects as an argument
-//will utilize the object's stored information by injecting it into the template
-var setCurrentAlbum = function(album) {
+    //Will take one our album objects as an argument
+    //will utilize the object's stored information by injecting it into the template
+    var setCurrentAlbum = function(album) {
 
-    var albumTitle = document.getElementsByClassName('album-view-title')[0];
-    var albumArtist = document.getElementsByClassName('album-view-artist')[0];
-    var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
-    var albumImage = document.getElementsByClassName('album-cover-art')[0];
-    var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+        var $albumTitle = $('.album-view-title');
+        var $albumArtist = $('.album-view-artist');
+        var $albumReleaseInfo =$('.album-view-release-info');
+        var $albumImage = $('.album-cover-art');
+        var $albumSongList = $('.album-view-song-list');
 
-    //nodeValue used to set the value of the text node
-    albumTitle.firstChild.nodeValue = album.title;
-    albumArtist.firstChild.nodeValue = album.artist;
-    albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
-    albumImage.setAttribute('src', album.albumArtUrl);
+        //nodeValue used to set the value of the text node
+        $albumTitle.text(album.title);
+        $albumArtist.text(album.artist);
+        $albumReleaseInfo.text(album.year + ' ' + album.label);
+        $albumImage.attr('src', album.albumArtUrl);
 
-    albumSongList.innerHTML = '';
+        $albumSongList.empty;
 
-    // to go through all the songs from the specified album object
-    //insert them into the HTML using the innerHTML property.
-    for (var i = 0; i < album.songs.length; i++) {
-         albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
-    }
-};
+        // to go through all the songs from the specified album object
+        //insert them into the HTML using the innerHTML property.
+        for (var i = 0; i < album.songs.length; i++) {
+            var $newRow = createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
+            $albumSongList.append($newRow);        
+        }
+    };
 
-// select the table containing all the songs in the album
-var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
+    // select the table containing all the songs in the album
+    var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
 
-// HTML Collection of all the songs in the album
-var songRows = document.getElementsByClassName('album-view-song-item');
+    // HTML Collection of all the songs in the album
+    var songRows = document.getElementsByClassName('album-view-song-item');
 
-var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
+    var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
 
-var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
+    var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
 
-//  // Store which song is playing
-var currentlyPlayingSong = null;
+    //  // Store which song is playing
+    var currentlyPlayingSong = null;
 
 // When we click to play, switch, or pause a song (anywhere in the row),
 //change the innerHTML of the element with the .song-item-number class to show play, pause, etc.
